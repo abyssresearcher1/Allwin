30b3721e0f88f0de006072bf45baefa1b0d690ff
abyssresearcher1 <akylbekashyralie@gmail.com>
2023-10-30 23:45:25 +0600

    burger menu done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,57 +4,38 @@ import cart from "../../assets/cart.png";
 import favourites from "../../assets/favourites.svg";
 import search from "../../assets/Search.png";
 import styles from "./Header.module.css";
+import cn from "classnames";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(!false);
-
-  const handleMenuToggle = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const closeMenu = () => {
-    setIsMenuOpen(!false);
-  };
-
-  const burgerMenuStyle = {
-    backgroundColor: isMenuOpen ? "transparent" : "black",
-  };
-
-  const headerStyle = {
-    display: isMenuOpen ? "block" : "none",
-  };
+  const [menu, setMenu] = useState(false);
 
   return (
     <div className={styles.Header}>
-      <div className={styles.burger__menu} style={burgerMenuStyle}>
-        <span>aalam</span>
-        <button onClick={handleMenuToggle} className={styles.openMenuBtn}>
-          {isMenuOpen ? "Close" : "Open"}
-        </button>
-      </div>
-      <header className={styles.header} style={headerStyle}>
+      <header className={styles.header}>
         <div className={styles.container}>
           <div className={styles.header__general}>
             <div className={styles.header__logo}>
               <span>aalam</span>
-            </div>
-            <div className={styles.navbar}>
-              <nav
-                className={`${styles.nav} ${isMenuOpen ? styles.openMenu : ""}`}
+              <button
+                className={styles.burgerBtn}
+                onClick={() => setMenu(!menu)}
               >
-                <Link to="/" onClick={closeMenu}>
-                  Главная
-                </Link>
-                <Link to="#" onClick={closeMenu}>
-                  Все товары
-                </Link>
-                <Link to="/About" onClick={closeMenu}>
-                  О нас
-                </Link>
-                <Link to="/Delivery" onClick={closeMenu}>
-                  Доставка
-                </Link>
-                <a href="/#fullfilment" onClick={closeMenu}>
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+            <div className={cn(styles.navbar, { [styles.headerStyle]: menu })}>
+              <nav className={styles.nav}>
+                <Link to="/">Главная</Link>
+                <Link to="#">Все товары</Link>
+                <Link to="/About">О нас</Link>
+                <Link to="/Delivery">Доставка</Link>
+                <a
+                  href="/#fullfilment"
+                  onClick={() => {
+                    setMenu(false);
+                  }}
+                >
                   Фулфилмент
                 </a>
               </nav>
@@ -68,14 +49,14 @@ const Header = () => {
                   />
                 </div>
                 <div className={styles.header__settings}>
-                  <Link to="/Cart" onClick={closeMenu}>
+                  <Link to="/Cart">
                     <img
                       src={cart}
                       alt="cartIcon"
                       className={styles.header__cart}
                     />
                   </Link>
-                  <Link to="/Favourites" onClick={closeMenu}>
+                  <Link to="/Favourites">
                     <img
                       src={favourites}
                       alt="favoritesIcon"
